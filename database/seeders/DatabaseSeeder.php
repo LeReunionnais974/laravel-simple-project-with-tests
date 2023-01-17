4f052e961a0cf5e4b70dc88b9ef17d1e19f7fab8
LeReunionnais974 <fabienpaul.grondin@gmail.com>
2023-01-17 12:13:04 +0100

    Ajout de la librairie Breeze

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,13 +15,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::create([
+            'first_name' => 'Admin',
+            'last_name' => 'Administrator',
+            'email' => 'admin@admin.fr',
+            'is_admin' => true,
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory(3)->create();
 
-        // \App\Models\Product::factory(6)->create();
+        \App\Models\Product::factory(6)->create();
     }
 }
